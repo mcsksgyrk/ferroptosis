2bc26d7055ce63d252f0eda7bd25c4726b09285a
mcsksgyrk <bbence.hajdu@gmail.com>
2025-03-15 09:51:50 +0100

    .

--- datawrangling/transform_ferrdb.py
+++ datawrangling/transform_ferrdb.py
@@ -10,10 +10,9 @@ from database.external_db import DBconnector
 
 ferrdb = DBconnector(OUTPUTS_DIR / "ferrdb.db")
 
-cols = ["UniProtAC","Symbol_or_reported_abbr","Exp_organism"]
+cols = ["UniProtAC", "Symbol_or_reported_abbr", "Exp_organism"]
 driver_prs_q = f"SELECT {', '.join(cols)} FROM driver WHERE Gene_type_hgnc_locus_type_or_other='gene with protein product'"
 driver_prs = ferrdb.custom_query(driver_prs_q)
 pr_df_cols = ['unbiprot_id', 'gene_name', 'topology']
 df_drivers = pd.DataFrame(data=driver_prs, columns=pr_df_cols)
-
-ferrdb.get_table_names()
+tables = ferrdb.get_table_names()
