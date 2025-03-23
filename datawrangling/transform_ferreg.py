e3b9120836340ddaa0daeb15f47a7475b0218775
mcskgyrk <bbence.hajdu@gmail.com>
2025-03-23 21:06:32 +0100

    for loop for ferreg

--- datawrangling/transform_ferreg.py
+++ datawrangling/transform_ferreg.py
@@ -6,9 +6,38 @@ from database.sqlite_db_api2 import PsimiSQL
 from apicalls.api_oop import PubChemClient
 
 
+def extractor(db: DBconnector,
+              columns: str,
+              table: str,
+              filters: Optional[List[str]] = None) -> List[str]:
+    query = db.compile_query_string(columns, table, filters)
+    res = db.custom_query(query)
+    return res
+
+
+ferreg = DBconnector(OUTPUTS_DIR / "ferreg.db")
+query_uniques = [
+    {"db": ferreg,
+     "table": "general_drug",
+     "id_column": "drug_name,drug_type,ttd_id,drugmap_id",
+     "params": ["Type='Protein coding'"],
+     "source": "ferreg",
+     "is_core": 0
+     }
+]
+ligand_cols_name = query_uniques[0]['id_column'].split(',')
+res = extractor(ferreg, query_uniques[0]["id_column"], query_uniques[0]['table'])
+ligand_df = pd.DataFrame(data=res, columns=ligand_cols_name)
+
+pubchem_client = PubChemClient()
+for _, row in ligand_df.iterrows():
+    for column, val in row.items():
+        res = pubchem_client.get_primary_cid_or_sid(val)
+        if isinstance(res, int):
+            print(res)
+            break
+
 SQL_SEED = PROJECT_ROOT/"database"/"network_db_seed2.sql"
 DB_DESTINATION = OUTPUTS_DIR/"ferreg_network.db"
 ferreg = DBconnector(OUTPUTS_DIR/"ferrdb.db")
 db_api = PsimiSQL(SQL_SEED)
-pubchem_client = PubChemClient()
-pubchem_client.get_primary_cid_or_sid("Tongxinluo")
