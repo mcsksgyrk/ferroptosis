45afc55334d9e3178d16a0a05452464e44dabf6d
Syspharma <syspharma@Syspharmas-MacBook-Pro.local>
2026-02-19 10:13:00 +0100

    nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {self, ...} @inputs:
+        inputs.flake-utils.lib.eachDefaultSystem (system:
+            let pkgs = inputs.nixpkgs.legacyPackages.${system};
+            in {
+                devShells.default = pkgs.mkShell {
+                    buildInputs = [
+                        pkgs.python314
+                        pkgs.poetry
+                        pkgs.pyright
+                        pkgs.sqlite
+                    ];
+
+                    shellHook = ''
+                        export POETRY_VIRTUALENVS_IN_PROJECT=true
+                        export POETRY_VIRTUALENVS_PATH="./.venv"
+
+                        if [ ! -f pyproject.toml ]; then
+                            echo "No pyproject.toml found. Initializing Poetry project..."
+                            poetry init --no-interaction --name="ferroptosis" --python="^3.14"
+                        fi
+
+                        if [ ! -d .venv ]; then
+                            echo "Creating poetry virtual environment"
+                            poetry install --no-root
+                        fi
+
+                        echo "Poetry env activated"
+                        echo "Python: $(python --version)"
+
+                        exec zsh -c "source '$(pwd)/.venv/bin/activate' && exec zsh"
+                    '';
+                };
+            }
+        );
+}
